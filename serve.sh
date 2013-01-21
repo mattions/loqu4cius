3e0db8669c993478a1b8eedd81aea2b2a9d3b23f
Michele Mattioni <mattions@gmail.com>
2013-01-21 15:47:19 +0000

    Adding the starting with mysql_user  stab.

--- serve.sh
+++ serve.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-dev_appserver.py . -d --use_sqlite --datastore_path=tmp/data
+#dev_appserver.py . -d --use_sqlite --datastore_path=tmp/data
+
+dev_appserver.py . --mysql_user=root -d --use_sqlite --datastore_path=tmp/data
