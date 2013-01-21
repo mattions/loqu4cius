5caa467105ab02145fd92ad03405479eb0362735
Michele Mattioni <mattions@gmail.com>
2013-01-21 15:44:48 +0000

    Setting up the dev vars

---
+++ development_on.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# Inserting googleappengine and Django in the path
+export PYTHONPATH=$PYTHONPATH:/home/mattions/Dev/Progs/google_appengine
+export PYTHONPATH=$PYTHONPATH:/home/mattions/Dev/Progs/google_appengine/lib/django_1_4/
+export PYTHONPATH=$PYTHONPATH:/home/mattions/Dev/locqu4cius/lib/
+
+# Activating the virtual env.
+echo "Activate the envriroment with: "
+echo "workon loqu4cius_env"
