2506ddcdf13e6df291a90544119db49c2aaf49b7
Michele Mattioni <mattions@gmail.com>
2013-01-21 18:43:16 +0000

    Improved the release script.

--- release_site.py
+++ release_site.py
@@ -2,22 +2,35 @@
 
 import subprocess
 import os
+import re 
 
-os.environ['SETTINGS_MODE'] = prod
+print "Changing SETTINGS_MODE changed in prod."
+os.environ['SETTINGS_MODE'] = "prod"
+
+print "Performing remote syncdb"
 
 subprocess.call(["./manage.py", "syncdb"])
-subprocess.call(["appcfg.py", "update", "."])
 
-#let add and tag the code
+
+## We get the version from the APP_YAML and increase the version automatically.
 APP_YAML = "app.yaml"
 
-f = open(APP_YAML, 'r')
+f = open(APP_YAML, 'w')
 lines = f.readlines()
 f.close()
 match = re.match("version: (\d+)", lines[1])
-version_number = m.group(1)
+version_number = match.group(1)
+version_number += 1
 version_tag = "v{0}".format(version_number)
+
+print "Releasing with appcfg"
+subprocess.call(["appcfg.py", "-V", version_number, "update", "."])
+
+#let add and tag the code
+
+print "Committing the changed app.yaml"
 subprocess.call(["git", "add", APP_YAML])
 subprocess.call(["git", "commit", "Update version for Release"])
 
 subprocess.call(["git", "tag", version_tag])
+print "Code tagged with: {0}".format(version_tag)
