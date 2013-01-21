df8d1166b45f625a8d8f434b2afb3e6d24a39302
Michele Mattioni <mattions@gmail.com>
2013-01-21 14:03:25 +0000

    Inserting the admin url

--- urls.py
+++ urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
+from django.contrib import admin
 
 urlpatterns = patterns(
     '',
     (r'^appengine_sessions/', include('appengine_sessions.urls')),
     (r'', include('core.urls')),
+    url(r"^admin/", include(admin.site.urls)),
     url(r"^home", TemplateView.as_view(template_name="homepage.html"), name="home"),
 
 )
