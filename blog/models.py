8752cf9874330357b6cceaa97e519350aae3a29a
Michele Mattioni <mattions@gmail.com>
2013-01-22 15:37:25 +0000

    Slugs should be unique in the db

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-# Create your models here.
+from django.template.defaultfilters import slugify
+from django.core.urlresolvers import reverse
 
 class Entry(models.Model):
     author = models.ForeignKey(User)
     title = models.CharField(max_length=255)
-    slug = models.SlugField()
+    slug = models.SlugField(unique=True)
     body_text = models.TextField()
-    pub_date = models.DateTimeField()
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.id:
+            # Newly created object, so set slug
+            self.slug = slugify(self.title)
+
+        super(Entry, self).save(*args, **kwargs)
+    
+    def get_absolute_url(self):
+        return reverse('entry-detail', args=[self.slug])
+        
     
