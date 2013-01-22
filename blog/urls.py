d9382c5f265f15be0cca705cefa279cd3d3a7fa6
Michele Mattioni <mattions@gmail.com>
2013-01-22 16:53:05 +0000

    Fixed the url order to have the proper name resolution

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ from .views import EntryList, EntryDetail, EntryCreate, EntryUpdate, EntryDelete
 
 urlpatterns = patterns('',
    url(r'post/create', EntryCreate.as_view(), name="entry-create"),
-   url(r'post/(?P<slug>[\w-]+)/', EntryDetail.as_view(), name="entry-detail"),
    url(r'post/(?P<slug>[\w-]+)/update', EntryUpdate.as_view(), name="entry-update"),
    url(r'post/(?P<slug>[\w-]+)/delete', EntryDelete.as_view(), name="entry-delete"),
+   url(r'post/(?P<slug>[\w-]+)/', EntryDetail.as_view(), name="entry-detail"),
    url(r'^', EntryList.as_view(), name='home'),
    )
