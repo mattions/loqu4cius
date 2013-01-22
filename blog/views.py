8f75979e3de21ae7f444fa15a498315f4a7c2c11
Michele Mattioni <mattions@gmail.com>
2013-01-22 15:40:27 +0000

    Saving the author of the post as the logged in user.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,19 @@
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.utils.timezone import now
 
 from braces.views import LoginRequiredMixin
 
 from .models import Entry
+from .forms import EntryForm
 
 class EntryCreate(LoginRequiredMixin, CreateView):
     model = Entry
+    form_class = EntryForm
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super(EntryCreate, self).form_valid(form)
 
 class EntryUpdate(LoginRequiredMixin, UpdateView):
     model = Entry
